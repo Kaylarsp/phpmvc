87f0fb243a9fd4638df2f3ffd3865cbcfa3629d5
Kayla <satitiputri.kayla@gmail.com>
2025-01-11 21:37:21 +0700

    add view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -6,10 +6,16 @@ class About extends Controller{
         $data['nama'] = $nama;
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
+        $data['judul'] = 'About';
+        $this->view('templates/header', $data);
         $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
         $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
