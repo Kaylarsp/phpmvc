6d8cba1a2d2a3949c525803aa07748db7df996d8
Kayla <satitiputri.kayla@gmail.com>
2025-01-13 21:47:17 +0700

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-    <h1>About Me</h1>
-    <p>Halo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+    <div class="container mt-4">
+        <h1>About Me</h1>
+        <img src="<?= BASEURL; ?>/img/profile.png" alt="kayla" width="200" class="rounded-5 shadow">
+        <p>Halo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+    </div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,10 @@
-    <h1>Selamat Datang di Website Saya</h1>
+    <div class="container">
+        <div class="p-5 mb-4 rounded-4 mt-5" style="background-color: #B3C8CF;">
+            <div class="container-fluid py-2" >
+                <h1 class="display-5 fw-bold">Selamat Datang di Website Saya!</h1>
+                <p>Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                <hr>
+                <button class="btn btn-sm btn-primary btn-lg" type="button">Example button</button>
+            </div>
+        </div>
+    </div>
